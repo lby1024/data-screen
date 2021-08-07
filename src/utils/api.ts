3f09d0b660fb89df918d04fec375bf856660fdec
boyang <1028524431@qq.com>
2021-08-07 19:16:30 +0800

    feat: form

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -3,7 +3,7 @@ export interface IData {
     series: number[]
 }
 
-function num(max: number, min=10) {
+export function num(max: number, min=10) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min; //不含最大值，含最小值
